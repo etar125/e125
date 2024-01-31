6a6abfd9d0dd59b560cf8512fce3d2d6cfc48464
etar125 <inniesharpsoftware@gmail.com>
2024-01-31 22:05:18 +0300

    Демонстрация новых инструкций

--- main.cpp
+++ main.cpp
@@ -6,13 +6,19 @@ using byte = unsigned char;
 int main()
 {
 
-	LoadToMemory(10, 4);
-	LoadToMemory(128, 5);
-	LoadToMemory(3, 6);
+	LoadToMemory(33, 4);
+	LoadToMemory(9, 5);
+	LoadToMemory(21, 6);
 	LoadToMemory(70, 7);
+	LoadToMemory(70, 8);
 	Run(4);
 
-	std::cout << (int)(GetFromMemory(128)) << std::endl;
+	std::cout << (int)(GetFromMemory(9)) << std::endl;
+
+	LoadToMemory(43, 4);
+	Run(4);
+
+	std::cout << (int)(GetFromMemory(9)) << std::endl;
 
 	return 0;
 }
